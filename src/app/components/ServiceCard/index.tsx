580c626bf1b92c538abc98de3d3461bd826c3813
Pedro Vinicius Wrublak <wrublakpedro@gmail.com>
2025-06-30 07:12:04 -0300

    Workink on service's crads

--- src/app/components/ServiceCard/index.tsx
+++ src/app/components/ServiceCard/index.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 
-function serviceCard() {
+type ServiceCardProps = {
+  image: string;
+  title: string;
+  description: string;
+};
+
+function serviceCard({image, title, description}: ServiceCardProps) {
   return (
     <section id="services" className="scroll-mt-28">
 
@@ -8,15 +14,15 @@ function serviceCard() {
     <div className=" flex flex-col bg-white rounded-2xl overflow-hidden hover:opacity-70 transition-shadow">
       <div className="relative h-[15rem]">
         <Image
-          src="/galery/living-room-ex.jpg"
+          src={image}
           alt="Imagem do mobiliário"
           fill
           className="object-cover z-0"
         />
       </div>
     <div className="p-6 text-center z-10 bg-amber-100 select-none">
-        <h2 className="text-2xl font-serif mb-2 text-black">SALA</h2>
-        <p className="font-serif text-black">Ambiente altamente personlizável</p>
+        <h2 className="text-2xl font-serif mb-2 text-black">{title}</h2>
+        <p className="font-serif text-black">{description}</p>
     </div>
     </div>
     </section>
--- src/app/components/Services/index.tsx
+++ src/app/components/Services/index.tsx
@@ -1,15 +1,49 @@
 import ServiceCard from "../ServiceCard";
 
+const services = [
+    {
+        image: "/galery/living-room-ex.jpg",
+        title: "SALA",
+        description: "Elegância, conforto e funcionalidade"
+    },
+    {
+        image: "/galery/kitchen-ex.JPEG",
+        title: "COZINHAS",
+        description: "Elegância, conforto e funcionalidade"
+    },
+    {
+        image: "/galery/living-room-ex.jpg",
+        title: "BANHEIROS",
+        description: "Elegância, conforto e funcionalidade"
+    },
+    {
+        image: "/galery/living-room-ex.jpg",
+        title: "DORMITÓRIOS",
+        description: "Elegância, conforto e funcionalidade"
+    },
+    {
+        image: "/galery/living-room-ex.jpg",
+        title: "LAVANDERIAS",
+        description: "Elegância, conforto e funcionalidade"
+    },
+    {
+        image: "/galery/living-room-ex.jpg",
+        title: "ESCRITÓRIOS",
+        description: "Elegância, conforto e funcionalidade"
+    },
+    
+]
+
 
 function Services() {
     return (
         <div className="flex flex-col items-center p-20 bg-white">
             <h1 className="text-4xl text-center font-serif mb-12">SERVIÇOS</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-5xl">
-                <ServiceCard />
-                <ServiceCard />
-                <ServiceCard />
-                <ServiceCard />
+                {services.map((item, index) => (
+                    <ServiceCard key={index} image={item.image} title={item.title} description={item.description} />
+                ))}
+                
             </div>
            
         </div>
