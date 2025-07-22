6cb58d64e6aaf2c96c2d435eabb6b8cfeeb81c62
Pedro Vinicius Wrublak <wrublakpedro@gmail.com>
2025-07-22 16:09:48 -0300

    Responsive adjusts on services section and homeimage

--- src/app/components/HomeImage/index.tsx
+++ src/app/components/HomeImage/index.tsx
@@ -3,9 +3,9 @@
 function HomeImage() {
   return (
     <div
-      className="w-full h-[30rem] md:h-[40rem] bg-fixed bg-center bg-cover bg-no-repeat"
+      className="w-full h-[25rem] md:h-[40rem] bg-fixed bg-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: "url('/home-image-ex (2).jpg')",
+        backgroundImage: "url('/home-image-ex-2.jpg')",
       }}
     />
   );
