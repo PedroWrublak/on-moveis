93edf37149651e597e35f259da3c6bb904805394
Pedro Vinicius Wrublak <wrublakpedro@gmail.com>
2025-07-22 16:17:08 -0300

    Removed bg-fixed on mobile

--- src/app/components/HomeImage/index.tsx
+++ src/app/components/HomeImage/index.tsx
@@ -3,7 +3,7 @@
 function HomeImage() {
   return (
     <div
-      className="w-full h-[25rem] md:h-[40rem] bg-fixed bg-center bg-cover bg-no-repeat"
+      className="w-full h-[25rem] md:h-[40rem] md:bg-fixed bg-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: "url('/home-image-ex-2.jpg')",
       }}
